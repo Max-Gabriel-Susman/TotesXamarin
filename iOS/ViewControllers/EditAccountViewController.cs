215297c7785f2bd16aad20615b14a29f2670c3f2
Moses <gabe.susman@gmail.com>
2020-07-25 16:23:39 -0600

    built new viewcontrollers to encompass additional functionality, classed the appropriate viewcontrollers in storyboards

---
+++ iOS/ViewControllers/EditAccountViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace totesXamarin.iOS.ViewControllers
+{
+    public partial class EditAccountViewController : UIViewController
+    {
+        public EditAccountViewController() : base("EditAccountViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
